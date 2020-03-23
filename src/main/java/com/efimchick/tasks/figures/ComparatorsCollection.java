4a4322db8490feb6b45bf2179c1ec5970cb5efde
thejerome <thejerome@mail.ru>
2020-03-23 20:41:54 +0300

    ComparatorsCollection skeleton

---
+++ src/main/java/com/efimchick/tasks/figures/ComparatorsCollection.java
@@ -0,0 +1,19 @@
+package com.efimchick.tasks.figures;
+
+public class ComparatorsCollection {
+
+    //TODO
+    public static int compareByArea(Figure lhs, Figure rhs){
+        return 0;
+    }
+
+    //TODO
+    public static int compareByHorizontalStartPosition(Figure lhs, Figure rhs){
+        return 0;
+    }
+
+    //TODO
+    public static int compareByHorizontalCenterPosition(Figure lhs, Figure rhs){
+        return 0;
+    }
+}
